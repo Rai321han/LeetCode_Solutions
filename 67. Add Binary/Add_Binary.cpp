1dd99dd4e3598db0f9dd18bfe3ece4ac72fdf1c6
Raihan Uddin <uddinraihan797@gmail.com>
2022-07-31 01:35:42 +0600

    Binary Add Solved

--- 67. Add Binary/Add_Binary.cpp
+++ 67. Add Binary/Add_Binary.cpp
@@ -11,23 +11,21 @@ string addBinary(string a, string b) {
 
   while (i >= 0 || j >= 0) {
     int sum = carry;
-    if (i >= 0) sum = sum + a[i] - '0';
-    if (j >= 0) sum = sum + b[j] - '0';
-    result = char(sum % 2) + result; //to not doing reverse I've added result after (sum%2)
-    cout<<result;
+    if (i >= 0) sum = sum + (a[i] - '0');
+    if (j >= 0) sum = sum + (b[j] - '0');
+    result = static_cast<char>(sum % 2 + '0') + result; //to not doing reverse I've added result after (sum%2)
     carry = sum / 2;
     i--;
     j--;
   }
 
-  if (carry > 0) result = char(carry) + result;
+  if (carry > 0) result = static_cast<char>(carry + '0') + result;
   return result;
 };
 
 int main() {
-    string a = "1001";
-    string b = "0";
-
+    string a = "11";
+    string b = "1";
     string res = addBinary(a,b);
     cout<<res;
     return 0;
