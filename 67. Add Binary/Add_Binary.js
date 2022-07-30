0ff582080e3bd038e3ec68f9629c573a5f6751d9
Raihan Uddin <uddinraihan797@gmail.com>
2022-07-31 00:59:21 +0600

    Binary Add solved

--- 67. Add Binary/Add_Binary.js
+++ 67. Add Binary/Add_Binary.js
@@ -8,15 +8,15 @@ var addBinary = function (a, b) {
     let sum = carry;
     if (i >= 0) sum = sum + Number(a[i]);
     if (j >= 0) sum = sum + Number(b[j]);
-    result += sum % 2;
+    result = (sum % 2) + result; //to not doing reverse I've added result after (sum%2)
     carry = parseInt(sum / 2);
     i--;
     j--;
   }
 
-  if (carry > 0) result += carry;
-  return [...result].reverse().join("");
+  if (carry > 0) result = carry + result;
+  return result;
 };
 
-let result = addBinary("0", "11101");
+let result = addBinary("101", "101");
 console.log(result);
