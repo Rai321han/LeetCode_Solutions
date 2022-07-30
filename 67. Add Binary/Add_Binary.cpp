78a3541e271ba8ff9326ba8be4fd1e27c0917808
Raihan Uddin <uddinraihan797@gmail.com>
2022-07-31 01:08:37 +0600

    Trying with C++

--- 67. Add Binary/Add_Binary.cpp
+++ 67. Add Binary/Add_Binary.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
-#include <vector>
 #include <string>
 
 using namespace std;
 
 string addBinary(string a, string b) {
-    int size_a = a.size();
-    int size_b = b.size();
-    int i = size_a; j = size_b;
-    while(i>=0 || j>=0; )
-}
+  int i = a.size() - 1;
+  int j = b.size() - 1;
+  int carry = 0;
+  string result = "";
+
+  while (i >= 0 || j >= 0) {
+    int sum = carry;
+    if (i >= 0) sum = sum + a[i] - '0';
+    if (j >= 0) sum = sum + b[j] - '0';
+    result = char(sum % 2) + result; //to not doing reverse I've added result after (sum%2)
+    cout<<result;
+    carry = sum / 2;
+    i--;
+    j--;
+  }
+
+  if (carry > 0) result = char(carry) + result;
+  return result;
+};
 
 int main() {
-    string a = "10110";
-    string b = "1001";
+    string a = "1001";
+    string b = "0";
 
-    addBinary(a,b);
+    string res = addBinary(a,b);
+    cout<<res;
     return 0;
 }
